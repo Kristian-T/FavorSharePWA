973328a234d405578b7d7b5954e284cd431e3b3a
Kristian Toft <bananatoft@gmail.com>
2020-10-13 13:57:40 +0200

    Base HTML

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,31 @@
+var requests =[
+    {"title":"Corona Shopping", "description":"Lorem ipsum"},
+    {"title":"Lift to center", "description":"Lorem ipsum"},
+    {"title":"Need printer access", "description":"Lorem ipsum"}
+];
+
+let grid = document.getElementById("grid");
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('/FavorSharePWA/public/sw.js')
         .then(function() { console.log('Service Worker Registered'); });
 }
 
-const fli = document.createElement("li");
-const fdiv = document.createElement("div");
+
+for(let req of requests) {
+    const rli = document.createElement("li");
+    const rdiv = document.createElement("div");
+    const rtitle = document.createElement("p");
+    const rdescription = document.createElement("p");
+
+    rtitle.innerHTML = req.title;
+    rdescription.innerHTML = req.description;
+
+    grid.appendChild(rli);
+    rli.appendChild(rdiv);
+    rdiv.appendChild(rtitle);
+    rdiv.appendChild(rdescription);
+}
+
+
