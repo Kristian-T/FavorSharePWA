2b42fbc2467c39890b2fb7e93a6558e3894d4c9a
FrederikHSt <frederikhstaal@gmail.com>
2020-10-13 13:55:55 +0200

    manifest plus serviceworker skulle virke

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,6 @@
-if ('serviceWorker' in navigator) {
+/*if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('/FavorSharePWA/public/sw.js')
         .then(function() { console.log('Service Worker Registered'); });
 }
+*/
